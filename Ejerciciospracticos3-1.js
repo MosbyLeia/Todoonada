ed717e3e9f1288b61def42e19162a7abca5f3a2c
MosbyLeia <mariaaureliablanco@gmail.com>
2022-04-19 02:30:40 -0300

    completo

---
+++ Ejerciciospracticos3-1.js
@@ -0,0 +1,82 @@
+//1
+
+var noValgoNi5 = 4;
+console.log(noValgoNi5);
+
+//2
+
+var miAnioDeNacimiento = 1987;
+var meGustariaTener80Anios = 80;
+var voyATener80ElAnio = miAnioDeNacimiento + meGustariaTener80Anios;
+console.log(voyATener80ElAnio);
+
+//3
+
+var costoDeUnaTele = 10000;
+var ahorros = 9000;
+var dineroQueMeFalta = costoDeUnaTele - ahorros;
+console.log(dineroQueMeFalta);
+
+//4
+
+var diasPorAnio = 365;
+var cantidadDeAniosEnUnaDecada = 10;
+var cantidadDeDiasEnUnaDecada = diasPorAnio * cantidadDeAniosEnUnaDecada;
+console.log(cantidadDeDiasEnUnaDecada);
+
+//5
+
+var pizza = 8;
+var porcionesEnLaMesa = 24;
+var pizzasTotales = porcionesEnLaMesa / pizza;
+console.log(pizzasTotales);
+
+//6
+
+var miAnimalFavorito = "gato";
+if(miAnimalFavorito == "perro"){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+//7
+
+var f = "flan";
+var d = "dulce de leche";
+if(f != d){
+    console.log(true);
+}
+
+//8
+var soyMayorDeEdad = 35 > 17;
+console.log(soyMayorDeEdad);
+
+//9
+
+var soyMenorDe25 = 35 < 25;
+console.log(soyMenorDe25);
+
+//10
+
+var estamosEnEnero = "enero" === "abril";
+console.log(estamosEnEnero);
+
+//11
+
+var noEstamosEnEnero = "enero" !== "abril";
+console.log(noEstamosEnEnero);
+
+//12
+
+var miNotaEnElParcial = 8;
+var notaMinimaParaAprobar = 6;
+var estoyAprobado = miNotaEnElParcial >= notaMinimaParaAprobar;
+console.log(estoyAprobado);
+
+
+//13
+
+var esHoraDeAlmorzar = 4 <=  12;
+console.log(esHoraDeAlmorzar);
