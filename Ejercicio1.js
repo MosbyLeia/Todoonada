88f647941c6334cfd1b6714ab1255d4af96cf7bf
MosbyLeia <73869310+MosbyLeia@users.noreply.github.com>
2022-06-06 00:08:13 -0300

    Add files via upload

---
+++ Ejercicio1.js
@@ -0,0 +1,9 @@
+var diaCumple = 19;
+var mesCumple = 02;
+var anioCumple = 1987;
+var numeroFavorito = 14;
+
+console.log(diaCumple);
+console.log(mesCumple);
+console.log(anioCumple);
+console.log(numeroFavorito);
---
+++ pruebajs.js
@@ -0,0 +1,10 @@
+<script>
+var x = 10;
+var y = 20;
+/* Test A */
+x = x + x + 1;
+y = y - y - x;
+
+alert("Valor de X: " + x);
+alert("Valor de Y: " + y);
+</script>
